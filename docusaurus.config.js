296e2c6dcfd8fe9aa9a29e44a2c1c151d7b214c7
jianglijun <jianglijun@digitalpredict.cn>
2023-10-25 21:07:34 +0800

    feat

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,7 +43,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/jianglijunde/visiondocs.github.io/tree/main/",
+            "https://github.com/jianglijunde/visiondocs.github.io/tree/gh-pages/",
           // "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         blog: {
