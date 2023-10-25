d3be382049169e75863af3ea2ba8c554bf8be0e1
jianglijun <jianglijun@digitalpredict.cn>
2023-10-25 20:56:54 +0800

    feat

---
+++ src/components/HomepageHeader/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import clsx from "clsx";
+import styles from "./styles.module.css";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Link from "@docusaurus/Link";
+
+type FeatureItem = {
+  title: string;
+  Svg: React.ComponentType<React.ComponentProps<"svg">>;
+  description: JSX.Element;
+};
+
+export default function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">魔狸科技</h1>
+        {/* <h1 className="hero__title">{siteConfig.title}</h1> */}
+        {/* <p className="hero__subtitle">{siteConfig.tagline}</p> */}
+        {/* <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro"
+          >
+            Docusaurus Tutorial - 5min ⏱️
+          </Link>
+        </div> */}
+      </div>
+    </header>
+  );
+}
