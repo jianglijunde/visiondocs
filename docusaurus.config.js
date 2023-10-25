7779b4ce0c0656bb03b469a302c87bfcecd3a261
jianglijun <jianglijun@digitalpredict.cn>
2023-10-25 16:01:43 +0800

    feat

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: "https://jianglijunde.github.io/visiondocs/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/visiondocs",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
